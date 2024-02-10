3f753d2d5a91c38debfa7f8bd4e0b2003019bdda
tanneratkin <tmanatkin@gmail.com>
2024-02-09 18:43:48 -0700

    first

---
+++ wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+
+    // set rate
+    let rate = 10;
+    $("#rate").html(`Rate:\t$${rate}`);
+
+    // calculate function
+    $("#calculateCharge").click(function () {
+
+        // define variables
+        let hours = parseFloat($("#hours").val());
+
+        // only calculate if positive number
+        if (hours > 0) {
+            $("#charge").html(`Total Charge:\t${hours * rate}`);
+        } else {
+            // clear input, focus, and alert user
+            $("#hours").val('');
+            $("#hours").focus();
+            alert("Please enter a positive number.");
+        }
+    });
+});
